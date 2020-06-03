c2b8b075e623310aec140021035040f587d43a50
Prerna Verma <prernaverma212@gmail.com>
2020-06-04 00:52:00 +0530

    user ReST API

--- src/main/java/ConfigReader.java
+++ src/main/java/ConfigReader.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 public class ConfigReader {
+
+    private String table1;
+    private String table2;
+
+    public String getTable1() {
+        return table1;
+    }
+
+    public String getTable2() {
+        return table2;
+    }
+
+    public void setTable1(String table1) {
+        this.table1 = table1;
+    }
+
+    public void setTable2(String table2) {
+        this.table2 = table2;
+    }
 }
