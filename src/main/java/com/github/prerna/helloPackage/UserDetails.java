7eb0d5309afad82f90450feccc71c6da870d0bec
Prerna Verma <prernaverma212@gmail.com>
2020-06-03 16:26:55 +0530

    hello World ReST API commit

---
+++ src/main/java/com/github/prerna/helloPackage/UserDetails.java
@@ -0,0 +1,50 @@
+package com.github.prerna.helloPackage;
+
+public class UserDetails {
+    private String firstName;
+    private String lastName;
+    private String city;
+
+    //fields constructor
+    public UserDetails(String firstName, String lastName, String city) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+    }
+
+    //Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    //toString method
+
+    @Override
+    public String toString() {
+        return "UserDetails{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
