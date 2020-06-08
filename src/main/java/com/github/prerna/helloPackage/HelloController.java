3e7a4a4b3fb1a67416fa1b47e680657f17943a64
Prerna Verma <prernaverma212@gmail.com>
2020-06-08 19:58:10 +0530

    master commit

---
+++ src/main/java/com/github/prerna/helloPackage/HelloController.java
@@ -0,0 +1,23 @@
+package com.github.prerna.helloPackage;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//controller
+@RestController
+public class HelloController {
+
+    //URI - /helloWorld
+    //@RequestMapping(method = RequestMethod.GET, path = "/helloWorld")
+    @GetMapping("/helloWorld")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
+    @GetMapping("/helloWorld-bean")
+    public UserDetails helloWorldBean(){
+        return new UserDetails("prerna", "verma", "bangalore");
+    }
+}
