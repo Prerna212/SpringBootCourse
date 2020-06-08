5b7c727b4cd922a2a4bfc791db98c6dd172adbdb
Prerna Verma <prernaverma212@gmail.com>
2020-06-09 00:11:10 +0530

    Internationalization completed

--- src/main/java/com/github/prerna/helloPackage/HelloController.java
+++ src/main/java/com/github/prerna/helloPackage/HelloController.java
@@ -1,23 +1,39 @@
 package com.github.prerna.helloPackage;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 //controller
 @RestController
 public class HelloController {
 
+    @Autowired
+    private ResourceBundleMessageSource messageSource;
+
     //URI - /helloWorld
     //@RequestMapping(method = RequestMethod.GET, path = "/helloWorld")
     @GetMapping("/helloWorld")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World!";
     }
 
     @GetMapping("/helloWorld-bean")
-    public UserDetails helloWorldBean(){
+    public UserDetails helloWorldBean() {
         return new UserDetails("prerna", "verma", "bangalore");
     }
+
+    @GetMapping("/helloWorld-i18n")
+    public String getMessageInI18NFormat(@RequestHeader(name = "Accept-Language", required = false) String locale) {
+        return messageSource.getMessage("label.hello", null, new Locale(locale));
+    }
+
+    @GetMapping("/helloWorld-i18n2")
+    public String getMessageInI18NFormat2() {
+        return messageSource.getMessage("label.hello", null, LocaleContextHolder.getLocale());
+    }
+
 }
