0ecd6e0e28e9224dbf9c57f165d835280e284e5e
Prerna Verma <prernaverma212@gmail.com>
2020-06-09 19:39:55 +0530

    Versioning implementation

---
+++ src/main/java/com/github/prerna/Controllers/UserCustomHeaderVersioningController.java
@@ -0,0 +1,58 @@
+package com.github.prerna.Controllers;
+
+import com.github.prerna.dtos.UserDtoV1;
+import com.github.prerna.dtos.UserDtoV2;
+import com.github.prerna.entities.User;
+import com.github.prerna.exception.UserNotFoundException;
+import com.github.prerna.services.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Min;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/versioning/headers/users")
+public class UserCustomHeaderVersioningController {
+
+        @Autowired
+        private UserService userService;
+
+        @Autowired
+        private ModelMapper modelMapper;
+
+        @GetMapping(value = "/{Id}", headers = "API-VERSION=1")
+        public UserDtoV1 getUserById(@PathVariable("Id") @Min(1) long id) throws UserNotFoundException {
+            Optional<User> userOptional = userService.getUserById(id);
+
+            if(!userOptional.isPresent()){
+                throw new UserNotFoundException("User doesn't exist");
+            }
+
+            User user = userOptional.get();
+
+            UserDtoV1 userDtoV1 = modelMapper.map(user, UserDtoV1.class);
+
+            return userDtoV1;
+        }
+
+        @GetMapping(value = "/{Id}", headers = "API-VERSION=2")
+        public UserDtoV2 getUserById2(@PathVariable("Id") @Min(1) long id) throws UserNotFoundException {
+            Optional<User> userOptional = userService.getUserById(id);
+
+            if(!userOptional.isPresent()){
+                throw new UserNotFoundException("User doesn't exist");
+            }
+
+            User user = userOptional.get();
+
+            UserDtoV2 userDtoV2 = modelMapper.map(user, UserDtoV2.class);
+
+            return userDtoV2;
+        }
+
+    }
--- src/main/java/com/github/prerna/dtos/UserDtoV1.java
+++ src/main/java/com/github/prerna/dtos/UserDtoV1.java
@@ -1,4 +1,104 @@
 package com.github.prerna.dtos;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.github.prerna.entities.Order;
+import com.github.prerna.entities.Views;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.util.List;
+
 public class UserDtoV1 {
+
+    private long id;
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String role;
+    private String ssn;
+    private List<Order> orders;
+
+    //no arugument constructor
+    public UserDtoV1() {
+    }
+
+    //argument constructor
+    public UserDtoV1(long id, String userName, String firstName, String lastName, String email, String role, String ssn, List<Order> orders) {
+        this.id = id;
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+        this.ssn = ssn;
+        this.orders = orders;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
 }
